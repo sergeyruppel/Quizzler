54f67a4fcd70b146702dfc46b747d7cd77e7e6c5
Sergey Ruppel <sergeyruppel@gmail.com>
2023-08-10 23:06:48 +0700

    Project created

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Sergey Ruppel on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
