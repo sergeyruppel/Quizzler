135036e1e85f6aaed0a895a411f632204288954f
Sergey Ruppel <sergeyruppel@gmail.com>
2023-08-11 19:13:52 +0700

    UI setup

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -7,9 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet private weak var trueButton: UIButton!
+    @IBOutlet private weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        questionLabel.text = "Four + Two is equal to Six"
+    }
+    
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    private func setupUI() {
+        trueButton.layer.cornerRadius = trueButton.frame.width * 0.05
+        trueButton.layer.borderWidth = 4.0
+        trueButton.layer.borderColor = UIColor.systemTeal.cgColor
+        falseButton.layer.cornerRadius = falseButton.frame.width * 0.05
+        falseButton.layer.borderWidth = 4.0
+        falseButton.layer.borderColor = UIColor.systemTeal.cgColor
     }
 }
