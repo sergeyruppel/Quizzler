474420081f1be3015ab74c170b9de8765c99eae4
Sergey Ruppel <sergeyruppel@gmail.com>
2023-08-13 21:39:09 +0700

    Setting up the project and showing the questions.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,29 +13,33 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var trueButton: UIButton!
     @IBOutlet private weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = ["Four + Two is equal to Six.",
+                "Five - Three is greater than One.",
+                "Three + Eight is less than Ten."]
+    var questionNumber = 0
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        questionLabel.text = "Four + Two is equal to Six"
+        updateUI()
     }
-    
-    
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
     }
-    
-    
-    
     
     private func setupUI() {
         trueButton.layer.cornerRadius = trueButton.frame.width * 0.05
-        trueButton.layer.borderWidth = 4.0
-        trueButton.layer.borderColor = UIColor.systemTeal.cgColor
+        trueButton.layer.borderWidth = 3.0
+        trueButton.layer.borderColor = UIColor.systemIndigo.cgColor
         falseButton.layer.cornerRadius = falseButton.frame.width * 0.05
-        falseButton.layer.borderWidth = 4.0
-        falseButton.layer.borderColor = UIColor.systemTeal.cgColor
+        falseButton.layer.borderWidth = 3.0
+        falseButton.layer.borderColor = UIColor.systemIndigo.cgColor
+    }
+    
+    private func updateUI() {
+        questionLabel.text = quiz[questionNumber]
     }
 }
